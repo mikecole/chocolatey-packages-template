e8a8e87eb0f2f841b8dedb759b040a02ec7e5c1d
Mike Cole <cole.mike@gmail.com>
2020-05-08 09:47:47 -0500

    Fixed MASE

--- automatic/microsoftazurestorageexplorer/tools/chocolateyInstall.ps1
+++ automatic/microsoftazurestorageexplorer/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = 'af24d5f339a1c2bcc910137f409577ab55e7366903ddc5b4b7cce9f0c9beee4d'
+$checksum = 'AF24D5F339A1C2BCC910137F409577AB55E7366903DDC5B4B7CCE9F0C9BEEE4D'
 
 $packageArgs = @{
-  packageName   = 'microsoftazurestorageexplorer'
-  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  fileType      = 'exe'
-  url           = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
-  silentArgs    = '/VERYSILENT'
-  validExitCodes= @(0)
-  softwareName  = 'microsoftazurestorageexplorer*'
-  checksum      = $checksum
-  checksumType  = 'sha256'
+  packageName    = 'microsoftazurestorageexplorer'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /ALLUSERS'
+  validExitCodes = @(0)
+  softwareName   = 'microsoftazurestorageexplorer*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
