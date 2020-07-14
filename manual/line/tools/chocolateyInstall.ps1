7fc88562ac732417a6ec9ed0a59c156e2710bc39
Mike Cole <cole.mike@gmail.com>
2020-07-14 10:03:32 -0500

    Updated Line

--- manual/line/tools/chocolateyInstall.ps1
+++ manual/line/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Line*'
-  checksum       = '612B1F8EB7B59B87F6D6B9751AB34522F6D2FA966A1F804EDC6ADCFCB7453415'
+  checksum       = '9D45F39B01425B663EEAE83C58FE2E9D4BB72DBD191BC402B4B544332C050688'
   checksumType   = 'sha256'
 }
 
