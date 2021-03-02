77c52e146fa4a021b86704c2e6b600bb9cc58ab3
mikecole <cole.mike@gmail.com>
2021-03-02 13:07:31 +0000

    AU: 1 updated - zoom [skip ci] https://gist.github.com/fc383d2a7d15fa7825ca98990d04357a

--- automatic/zoom/tools/chocolateyInstall.ps1
+++ automatic/zoom/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
  
-$checksum = 'aee7b00bc50adf30b79f428515a61d48bd9a5393983b275c7c9fdc2a93177473'
-$url = 'https://zoom.us/client/5.5.12494.0204/ZoomInstallerFull.msi'
+$checksum = '4ed05da0edff5fe70fa740b14bcdead03377ed19475145dc42ae205480bf858c'
+$url = 'https://zoom.us/client/5.5.13142.0301/ZoomInstallerFull.msi'
 
 $packageArgs = @{
   packageName    = 'zoom-client'
