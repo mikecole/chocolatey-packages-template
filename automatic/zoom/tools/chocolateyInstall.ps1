20c8432c4097837f2bcfffdba9a885a33e81bd55
Mike Cole <cole.mike@gmail.com>
2021-04-23 10:02:38 -0500

    Adjusted zoom autoupdater

--- automatic/zoom/tools/chocolateyInstall.ps1
+++ automatic/zoom/tools/chocolateyInstall.ps1
@@ -2,12 +2,15 @@
 $checksum = '1C8E0F3DC13C2ED71A001B0ACB0B393CA2307198E7C4D1865697B3EF00CF083D'
 $checksum64 = 'F725BCA0198B470568AB65356A151C47B66985752CB7672569A7F7AFEBDA79DA'
 
+$url = 'https://cdn.zoom.us/prod/5.6.1.617/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/5.6.1.617/x64/ZoomInstallerFull.msi'
+
 $packageArgs = @{
   packageName    = 'zoom-client'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
-  url            = 'https://cdn.zoom.us/prod/5.6.1.617/ZoomInstallerFull.msi'
-  url64          = 'https://cdn.zoom.us/prod/5.6.1.617/x64/ZoomInstallerFull.msi'
+  url            = $url
+  url64          = $url64
   silentArgs     = '/quiet /norestart ZoomAutoUpdate=True'
   validExitCodes = @(0)
   softwareName   = 'zoom*'
